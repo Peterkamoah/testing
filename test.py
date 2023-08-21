83222c8fb6ec672e8d00475a316eb95839e63a6a
jacky040 <116127513+jackysmith040@users.noreply.github.com>
2023-08-21 13:14:53 +0000

    added function
    i redefined the long statements to a function

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-# Variables
-failed_subject="2"
-name='Jace' 
+def introduce(name, failed_subject)
+    print('Dear Mr Carson')
+    print('Your son ' + name  + ' is failing ' + failed_subject + ' subjects.')
+    print(name + ' will need to redo ' + failed_subject +  ' courses.')
+    name="Wesley"
+    print(name + ' is doing well in goegraphy ')
 
-# Printing
-print('Dear Mr Carson')
-print('Your son ' + name  + ' is failing ' + failed_subject + ' subjects.')
-print(name + ' will need to redo ' + failed_subject +  ' courses.')
-name="Wesley"
-print(name + ' is doing well in goegraphy ')
+introduce("John", 25)
