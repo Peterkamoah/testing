57e188a6a1618cb98d0d599f86781c48b686e850
Peterkamoah <142802106+Peterkamoah@users.noreply.github.com>
2023-08-21 12:49:12 +0000

    code updated

---
+++ test.py
@@ -0,0 +1,7 @@
+failed_subject="2"
+name='Jace' 
+print('Dear Mr Carson')
+print('Your son ' + name  + ' is failing ' + failed_subject + ' subjects.')
+print(name + ' will need to redo ' + failed_subject +  ' courses.')
+name="Wesley"
+print(name + ' is doing well in goegraphy ')
