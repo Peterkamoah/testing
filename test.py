36f2e4537e45ecffea4424b8cb94ab4ae1d774a4
Peterkamoah <142802106+Peterkamoah@users.noreply.github.com>
2023-08-21 13:16:17 +0000

    added to the code

--- test.py
+++ test.py
@@ -8,3 +8,4 @@ print('Your son ' + name  + ' is failing ' + failed_subject + ' subjects.')
 print(name + ' will need to redo ' + failed_subject +  ' courses.')
 name="Wesley"
 print(name + ' is doing well in goegraphy ')
+print(name + ' can help your son ' )
